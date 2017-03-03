efc31dc71b612fae9ca48d9664576f485df679c4
JyHu <auu.aug@gmail.com>
2017-03-03 18:28:51 +0800

    Add one property called , it allows you get the selected info all the time .

--- HMSegmentedControl/HMSegmentedControl.h
+++ HMSegmentedControl/HMSegmentedControl.h
@@ -224,6 +224,15 @@ typedef NS_ENUM(NSInteger, HMSegmentedControlImagePosition) {
 @property (nonatomic, readwrite) CGFloat selectionIndicatorHeight;
 
 /**
+ Whether need multiple selection. 
+ 
+ If set yes, it means that we will send the selected info all the time while user selected one index.
+ 
+ But first off all, you need set `touchEnabled` to yes
+ */
+@property (assign, nonatomic) BOOL needMultipleSelection;
+
+/**
  Edge insets for the selection indicator.
  NOTE: This does not affect the bounding box of HMSegmentedControlSelectionStyleBox
  
